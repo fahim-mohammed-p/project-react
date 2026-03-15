d5bcc6187dece602f0f029061852978dc0abad6a
fahim-mohammed-p <pfahim828@gmail.com>
2026-03-15 16:28:51 +0530

    server.json

--- project/api/server.js
+++ project/api/server.js
@@ -1,13 +1,22 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const path = require('path');
-const router = jsonServer.router(path.join(__dirname, '../db.json'));
+const fs = require('fs');
+
 const middlewares = jsonServer.defaults();
 
+// db.json file correct aayi locate cheyyaan
+const filePath = path.join(process.cwd(), 'db.json');
+const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+const router = jsonServer.router(data);
+
 server.use(middlewares);
+
+// Rewriting /api calls
 server.use(jsonServer.rewrites({
   '/api/*': '/$1'
 }));
+
 server.use(router);
 
 module.exports = server;
