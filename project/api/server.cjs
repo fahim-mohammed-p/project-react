7571ce438cfbb67f05f14925f22c0e4a3a90f166
fahim-mohammed-p <pfahim828@gmail.com>
2026-03-15 23:55:49 +0530

    server.cjs

--- project/api/server.cjs
+++ project/api/server.cjs
@@ -1,14 +1,11 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const path = require('path');
-const fs = require('fs');
 
 const middlewares = jsonServer.defaults();
 
-// db.json file correct aayi locate cheyyaan
-const filePath = path.join(process.cwd(), 'db.json');
-const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-const router = jsonServer.router(data);
+// Vercel-il direct aayi path kodukkunnathaanu safe
+const router = jsonServer.router(path.join(__dirname, '../db.json'));
 
 server.use(middlewares);
 
